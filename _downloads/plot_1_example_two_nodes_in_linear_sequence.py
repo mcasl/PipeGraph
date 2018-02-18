2fa90b51c0bc117f97d102130ed7238f982d1b51
mcasl <manuel.castejon@gmail.com>
2018-02-18 02:48:59 +0000

    Pushing the docs for revision for branch: master, commit 56c2f54951fb2ec90035ca0cc507743b360e629e

--- _downloads/plot_1_example_two_nodes_in_linear_sequence.py
+++ _downloads/plot_1_example_two_nodes_in_linear_sequence.py
@@ -50,11 +50,11 @@ steps = [('scaler', scaler),
 # - The keys of the top level entries of the dictionary must be the same as those of the previously defined steps.
 # - The values assocciated to these keys define the variables from other steps that are going to be considered as inputs for the current step. They are dictionaries themselves, where:
 #
-#     - The keys of the nested dictionary represent the input variables as named at the current step.
-#     - The values assocciated to these keys define the steps that hold the desired information and the variables as named at that step. This information can be written as:
+#   - The keys of the nested dictionary represent the input variables as named at the current step.
+#   - The values assocciated to these keys define the steps that hold the desired information and the variables as named at that step. This information can be written as:
 #
-#         - A tuple with the label of the step in position 0 followed by the name of the output variable in position 1.
-#         - A string representing a variable from an external source to the :class:`PipeGraphRegressor` object, such as those provided by the user while invoking the ``fit``, ``predict`` or ``fit_predict`` methods.
+#     - A tuple with the label of the step in position 0 followed by the name of the output variable in position 1.
+#     - A string representing a variable from an external source to the :class:`PipeGraphRegressor` object, such as those provided by the user while invoking the ``fit``, ``predict`` or ``fit_predict`` methods.
 #
 # For instance, the linear model accepts as input ``X`` the output named ``predict`` at the ``scaler`` step, and as input ``y`` the value of ``y`` passed to ``fit``, ``predict`` or ``fit_predict`` methods.
 
