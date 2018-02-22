75e526dccfa900863d5442264fcbdd60bab1b2ee
Laura Fernandez-Robles <l.fernandez@unileon.es>
2018-02-22 19:26:14 +0100

    small changes in example4

--- examples/plot_4_example_combination_of_classifiers.py
+++ examples/plot_4_example_combination_of_classifiers.py
@@ -90,4 +90,4 @@ plt.show()
 
 ###############################################################################
 # This example displayed complex data injections that are successfully managed by **PipeGraph**.
-# :ref:`Next example on <example5>`
+# Next example :ref:`on <example5>`.
