f12f7de32706244b9ea75945946f9a9a16261385
HECTOR <hector.moreton@unileon.es>
2018-03-08 11:50:32 +0100

    changed travis and install

--- ci_scripts/travis/install.sh
+++ ci_scripts/travis/install.sh
@@ -38,3 +38,4 @@ python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 python setup.py develop
+python setup.py sdist
