9227c2e295a16f58cea2e83a429f55fca3b2758d
mcasl <manuel.castejon@gmail.com>
2018-12-11 21:29:04 +0000

    Pushing the docs for revision for branch: , commit 3e37b5eee08e35c105a1eddb35b2c354256716ef

---
+++ _downloads/2cfd0331d56e9574840a141847cfb71e/plot_5_example_Demultiplexer_Multiplexer.py
@@ -0,0 +1,85 @@
+"""
+.. _example5:
+
+Fifth Example: Demultiplexor - multiplexor
+-----------------------------------------------
+
+An imaginative layout using a classifier to predict the cluster labels and fitting a separate model for each cluster.
+
+Steps of the **PipeGraph**:
+
+- **scaler**: A :class:`MinMaxScaler` data preprocessor
+- **classifier**: A :class:`GaussianMixture` classifier
+- **demux**: A custom :class:`Demultiplexer` class in charge of splitting the input arrays accordingly to the selection input vector
+- **lm_0**: A :class:`LinearRegression` model
+- **lm_1**: A :class:`LinearRegression` model
+- **lm_2**: A :class:`LinearRegression` model
+- **mux**: A custom :class:`Multiplexer` class in charge of combining different input arrays into a single one accordingly to the selection input vector
+
+.. figure:: https://raw.githubusercontent.com/mcasl/PipeGraph/master/examples/images/Diapositiva5.png
+
+    Figure 1. PipeGraph diagram showing the steps and their connections
+"""
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import GridSearchCV
+from pipegraph.base import PipeGraph, Demultiplexer, Multiplexer
+import matplotlib.pyplot as plt
+from sklearn.mixture import GaussianMixture
+
+X_first = pd.Series(np.random.rand(100,))
+y_first = pd.Series(4 * X_first + 0.5*np.random.randn(100,))
+X_second = pd.Series(np.random.rand(100,) + 3)
+y_second = pd.Series(-4 * X_second + 0.5*np.random.randn(100,))
+X_third = pd.Series(np.random.rand(100,) + 6)
+y_third = pd.Series(2 * X_third + 0.5*np.random.randn(100,))
+
+X = pd.concat([X_first, X_second, X_third], axis=0).to_frame()
+y = pd.concat([y_first, y_second, y_third], axis=0).to_frame()
+
+scaler = MinMaxScaler()
+gaussian_mixture = GaussianMixture(n_components=3)
+demux = Demultiplexer()
+lm_0 = LinearRegression()
+lm_1 = LinearRegression()
+lm_2 = LinearRegression()
+mux = Multiplexer()
+
+
+steps = [('scaler', scaler),
+         ('classifier', gaussian_mixture),
+         ('demux', demux),
+         ('lm_0', lm_0),
+         ('lm_1', lm_1),
+         ('lm_2', lm_2),
+         ('mux', mux), ]
+
+###############################################################################
+# Instead of using ``inject`` as in previous example, in this one we are going to pass a dictionary
+# describing the connections to PipeGraph constructor
+
+connections = { 'scaler': {'X': 'X'},
+                'classifier': {'X': 'scaler'},
+                'demux': {'X': 'scaler',
+                          'y': 'y',
+                          'selection': 'classifier'},
+                'lm_0': {'X': ('demux', 'X_0'),
+                         'y': ('demux', 'y_0')},
+                'lm_1': {'X': ('demux', 'X_1'),
+                         'y': ('demux', 'y_1')},
+                'lm_2': {'X': ('demux', 'X_2'),
+                         'y': ('demux', 'y_2')},
+                'mux': {'0': 'lm_0',
+                        '1': 'lm_1',
+                        '2': 'lm_2',
+                        'selection': 'classifier'}}
+
+pgraph = PipeGraph(steps=steps, fit_connections=connections)
+pgraph.fit(X, y)
+#%%
+y_pred = pgraph.predict(X)
+plt.scatter(X, y)
+plt.scatter(X, y_pred)
+
---
+++ _downloads/98e6283c8f313caeeedfb5c03f1c2f95/plot_Finding_Number_of_clusters.py
@@ -0,0 +1,91 @@
+"""
+.. _example_finding_the_number_of_clusters:
+
+Finding number of clusters
+-----------------------------------------
+
+ A two step PipeGraph is built by embedding a clustering algorithm such as Kmeans followed by
+ a supervised classification algorithm such as Linear Discriminant Analysis. This kind of structure
+ is useful in those scenarios in which the dataset comprises different subpopulations
+ but the class labels are unknown. A common question that arises in such scenario is related to
+ the best number of clusters. For answering that question, we might want to measure the quality
+ of the clustering in accordance to the later capability of reproducing the results obtained by
+ the clustering algorithm on a model based classification technique such as linear discriminant analysis.
+
+ Such PipeGraph is fit using a GridSearchCV strategy and an artificial dataset.
+"""
+#%%
+import matplotlib.pyplot as plt
+import numpy as np
+from pipegraph import PipeGraph
+from sklearn import datasets
+from sklearn.cluster import KMeans
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+import seaborn as sns; sns.set()
+
+X, y = datasets.make_blobs(n_samples=10000, n_features=5, centers=10)
+
+clustering = KMeans(n_clusters=10)
+# The values of the parameters in the initialization do not affect the grid search.
+
+classification = LinearDiscriminantAnalysis()
+
+steps = [('clustering', clustering),
+         ('classification', classification)
+         ]
+
+model = PipeGraph(steps=steps)
+model.inject(sink='clustering',     sink_var='X', source='_External',  source_var='X')
+model.inject(sink='classification', sink_var='X', source='_External',  source_var='X')
+model.inject(sink='classification', sink_var='y', source='clustering', source_var='predict')
+
+n_folds = 5
+number_of_clusters_to_explore = np.arange(2, 20)
+search = GridSearchCV(model, param_grid=dict(clustering__n_clusters=number_of_clusters_to_explore), cv=n_folds)
+search.fit(X)
+#%%
+###############################################################################
+# The results show the CV scores along with a fill area pointing out the standard deviation bands.
+
+# Code inspired by http://scikit-learn.org/stable/auto_examples/exercises/plot_cv_diabetes.html#sphx-glr-download-auto-examples-exercises-plot-cv-diabetes-py
+
+number_of_clusters = search.cv_results_['param_clustering__n_clusters'].data.astype(int)
+errors = search.cv_results_['mean_test_score']
+deviations = search.cv_results_['std_test_score']/np.sqrt(n_folds)
+
+plt.figure().set_size_inches(8, 6)
+plt.plot(number_of_clusters, errors)
+plt.plot(number_of_clusters, errors + deviations, 'b--')
+plt.plot(number_of_clusters, errors - deviations, 'b--')
+plt.fill_between(number_of_clusters, errors + deviations, errors - deviations, alpha=0.2)
+plt.ylabel('CV score +/- std error')
+plt.xlabel('Number of clusters')
+plt.axhline(np.max(errors), linestyle='--', color='.5')
+plt.xlim([number_of_clusters_to_explore[0], number_of_clusters_to_explore[-1]])
+plt.show()
+#%%
+###############################################################################
+# The results show an abrupt slope right after 10 clusters, which is commonly interpreted as
+# a hint for maximum number of clusters.
+#
+# The accuracy and confussion matrix for the 10 clusters configuration are shown below.
+
+# Code for plotting the confusion matrix taken from 'Python Data Science Handbook' by Jake VanderPlas
+
+model.fit(X)
+score = model.score(X)
+print('Accuracy:',score)
+kmeans_labels = model._predict_data[('clustering', 'predict')]
+prediction = model.predict(X)
+
+mat = confusion_matrix(y_true=kmeans_labels, y_pred=prediction)
+sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False)
+plt.xlabel('KMeans label')
+plt.ylabel('LDA predicted label');
+plt.show()
+#%%
+
+
+
